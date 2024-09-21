261a5ba608447baaf4633e077951580a7eee70dc
ErbITservices <erbitservces@gmail.com>
2024-09-21 22:05:06 +0530

    first commit

---
+++ models/Libraraylist.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const LibraraylistSchema = new Schema(
+  {
+    lname: { type: String, required: true },
+    startdate: { type: String, required: true },
+    enddate: { type: String, required: true },
+    amount: { type: String, required: true },
+    service: { type: String, require: true },
+  },
+  { timestamps: true }
+);
+
+
+const Libraraylist = mongoose.model("Libraraylist", LibraraylistSchema);
+module.exports = Libraraylist;
